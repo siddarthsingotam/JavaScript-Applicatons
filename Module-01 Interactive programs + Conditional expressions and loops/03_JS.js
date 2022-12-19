65d222cb37eb90647db968ac38322880e80100d9
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-12-20 02:22:12 +0530

    Fixed bug 03

---
+++ Module-01 Interactive programs + Conditional expressions and loops/03_JS.js
@@ -0,0 +1,11 @@
+let number1 = parseInt(prompt("Enter number 1: "));
+let number2 = parseInt(prompt("Enter number 2: "));
+let number3 = parseInt(prompt("Enter number 3: "));
+
+console.log(+number1)
+console.log(+number2)
+console.log(+number3)
+
+let sum = number1 + number2 + number3;
+let pro = number1 * number2 * number3;
+let avg = (number1 + number2 + number3) / 3;
