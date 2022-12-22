34d4923ffc6bc05faeefdcd8275db917f8a8daf5
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-12-22 13:13:17 +0530

    t3 module verified

--- Module-03 BOM, DOM and events/module3-starters-main/module3-starters-main/t3/3.js
+++ Module-03 BOM, DOM and events/module3-starters-main/module3-starters-main/t3/3.js
@@ -1,2 +1,9 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const ul = document.getElementById('target')
+
+for (let i = 0; i < names.length; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = `<li>${names[i]}</li>`
+    ul.appendChild(li)
+}
